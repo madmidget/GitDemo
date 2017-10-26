3ea14236ae37eb01a54a03d72bebd82b10b9c313
madmidget <32322793+madmidget@users.noreply.github.com>
2017-10-26 11:13:15 +0200

    Tog bort "Comments"

---
+++ Launcher.java
@@ -0,0 +1,20 @@
+
+public class Launcher {
+
+
+    public static void main (String[] arg){
+
+        GitHubDemo g = new GitHubDemo();
+
+        g.getClass();
+
+        System.out.println(g);
+
+        GitHubDemo g2 = new GitHubDemo();
+
+        g2.CountTo10();
+
+
+
+    }
+}
