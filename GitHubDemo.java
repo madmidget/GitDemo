b7aaa140b22f6349f9c904f2d76d84ec7b6fcbe2
madmidget <32322793+madmidget@users.noreply.github.com>
2017-10-26 11:27:24 +0200

    Tog bort "Comments" igen

---
+++ GitHubDemo.java
@@ -0,0 +1,27 @@
+
+class GitHubDemo {
+
+    String s;
+    int i;
+
+
+
+    public GitHubDemo(){
+
+        s = "Hello Github";
+        i = 10;
+
+    }
+    public String GitHudemo(){
+        return s;
+    }
+
+
+    public void CountTo10() {
+
+        for (int j=0; j<10; j++){
+            System.out.println(j);
+        }
+    }
+}
+
